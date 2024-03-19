b1e9caa6a378b8621f55e8a76bc84a4a074e5984
santaevpavel <santaevp@gmail.com>
2024-03-19 22:26:46 +0200

    update ConsoleMetricsDispatcher

--- plugin-core-extensions/src/main/kotlin/com/santaev/gradle_metrics_plugin/dispatcher/ConsoleMetricsDispatcher.kt
+++ plugin-core-extensions/src/main/kotlin/com/santaev/gradle_metrics_plugin/dispatcher/ConsoleMetricsDispatcher.kt
@@ -19,16 +19,16 @@ class ConsoleMetricsDispatcher : IMetricsDispatcher {
 
     override fun dispatch(metrics: List<Metric>) {
         val output = buildString {
-            appendln("\t-----------------------------------------------")
-            appendln("\t--------------- Metrics output ----------------")
-            appendln(String.format("\t%-25s %-10s %s", "-------------------------", "----------", "----------"))
-            appendln(String.format("\t%-25s %-10s %s", "Metric id", "Value", "Unit"))
-            appendln(String.format("\t%-25s %-10s %s", "-------------------------", "----------", "----------"))
+            appendLine("-----------------------------------------------")
+            appendLine("--------------- Metrics output ----------------")
+            appendLine(String.format("%-25s %-10s %s", "-------------------------", "----------", "----------"))
+            appendLine(String.format("%-25s %-10s %s", "Metric id", "Value", "Unit"))
+            appendLine(String.format("%-25s %-10s %s", "-------------------------", "----------", "----------"))
             metrics.forEach { metric ->
-                appendln(String.format("\t%-25s %-10s %s", metric.id, metric.value, metric.unit.name))
+                appendLine(String.format("%-25s %-10s %s", metric.id, metric.value, metric.unit.name))
             }
-            appendln(String.format("\t%-25s %-10s %s", "-------------------------", "----------", "----------"))
-            appendln()
+            appendLine(String.format("%-25s %-10s %s", "-------------------------", "----------", "----------"))
+            appendLine()
         }
         logger.lifecycle(output)
     }
